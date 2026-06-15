6d6a536af669c963589b1f25a452edde7a4a427e
Ahmed <ahmedhaque06@gmail.com>
2026-06-15 12:04:56 +0400

    Vercel fix

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+echo "Building Django app for Vercel..."
+
+# Install dependencies using pip (not uv)
+pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+
+# Run Django collectstatic
+python manage.py collectstatic --noinput
+
+echo "Build completed!"
